2054eae4be1789b80e03300591d8b66d3dc0ca7d
Diego_Flechas <diego.parra@ci2.co>
2023-05-24 16:52:50 -0500

    Realizado el punto 2

--- lógica de programación/index.php
+++ lógica de programación/index.php
@@ -1,4 +1,9 @@
 <?php 
+
+/* punto 1. Dado un número n encontrar todos los múltiplos de 3 y 5 menores al número dado, el
+ método debe retornar la suma de los múltiplos encontrados. Ejemplo: si el número
+ ingresado es 10, los múltiplos de 3 y 5 menores a 10 son: 3, 5, 6, 9, el resultado de la
+ función debe ser 23, debido a que es la suma de 3, 5, 6, 9.*/
 class Logica{
 
     /** 
@@ -15,6 +20,27 @@ class Logica{
     }
 }
 $Logica = new Logica();
-$num = 50;
+$num = 20;
 echo($Logica->sacarMultiplos($num));
+
+///////////////////////////////////////////////////////////
+
+/*punto 2. Dada una frase, devolver la frase donde las palabras con mayor a 5 letras estén al
+revés. Ejemplo: Dado “Bienvenido a Treda Solutions” retornar “odinevneiB a Treda
+snoituloS” */
+
+    function invertirFrase($frase){
+        $palabras = explode(" ", $frase);
+
+        foreach($palabras as $indice => $palabra){
+            if(strlen($palabra) > 5){
+            $palabras[$indice] = strrev($palabra);
+            }
+        }
+        return $fraseInvertida = implode(" ", $palabras);
+}
+
+$frase = "Bienvenido a Treda Solutions";
+$fraseInvertida = invertirFrase($frase);
+echo("<br>$fraseInvertida");
 ?>
